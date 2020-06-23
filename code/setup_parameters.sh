045b80c934cddca27817263974b56d576ef34920
Maximilian Rutz <maximilian.rutz@student.uni-tuebingen.de>
2020-06-23 10:31:12 +0200

    changed parameters to Raducan paper values

--- code/setup_parameters.sh
+++ code/setup_parameters.sh
@@ -1,9 +1,9 @@
 ## create setups with different parameters
-for porosity in 0.0 0.25 0.5 0.75
+for porosity in 0.0 0.17 0.33 0.5
 do
     for strength in 1e3 1e4 1e5 1e6
     do
-        for angle in 0 15 30 45
+        for angle in 0 45
         do
             python3 setup_simulation.py --porosity $porosity --strength $strength --angle $angle --name "por${porosity}_str${strength}_ang${angle}"
         done
