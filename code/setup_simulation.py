e5ea1963a691ba9fd8e9c32dbbd978dab4e4cf61
Maximilian Rutz <maximilian.rutz@student.uni-tuebingen.de>
2020-06-23 11:50:11 +0200

    consistency

--- code/setup_simulation.py
+++ code/setup_simulation.py
@@ -75,7 +75,7 @@ f.write(
 
 f.write(
     "## Creating initial input file\n"
-    f"##python3 ../../../impact_ini/impact_ini.py --outfile impact_{args.name}.0000 --angle {args.angle} --alpha_targ {1.0 / (1.0 - args.porosity)} --N_targ_des 300000 --R_targ 20.0 --R_targ_inner 5.0 --distance 5.0 --output_format 'SOLIDPOROUS' --R_proj 0.36 --sml_fact 2.1 --weibull_m 16.0 --weibull_k 1e61 --damage 0.0 --stress 0.0 --alpha_proj 1.0 --pressure 0.0\n\n"
+    f"#python3 ../../../impact_ini/impact_ini.py --outfile impact_{args.name}.0000 --angle {args.angle} --alpha_targ {1.0 / (1.0 - args.porosity)} --N_targ_des 300000 --R_targ 20.0 --R_targ_inner 5.0 --distance 5.0 --output_format 'SOLIDPOROUS' --R_proj 0.36 --sml_fact 2.1 --weibull_m 16.0 --weibull_k 1e61 --damage 0.0 --stress 0.0 --alpha_proj 1.0 --pressure 0.0\n\n"
     f"python3 ../../../impact_ini/impact_ini.py --outfile impact_{args.name}.0000 --angle {args.angle} --alpha_targ {1.0 / (1.0 - args.porosity)} --N_targ_des 30000 --R_targ 10.0 --R_targ_inner 1.0 --distance 2.5 --output_format 'SOLIDPOROUS' --R_proj 0.36 --sml_fact 2.1 --weibull_m 16.0 --weibull_k 1e61 --damage 0.0 --stress 0.0 --alpha_proj 1.0 --pressure 0.0\n\n"
     f"#cp ../../code/impact.0000 impact_{args.name}.0000\n\n"
 )
